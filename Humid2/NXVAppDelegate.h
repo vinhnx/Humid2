3758fd56012e3040ec183c5ace9407f0aaa85d28
Vinh Nguyen <vinhnguyen2308@gmail.com>
2014-03-17 14:20:37 +0700

    initial commit

---
+++ Humid2/NXVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NXVAppDelegate.h
+//  Humid2
+//
+//  Created by Vinh Nguyen on 3/17/14.
+//  Copyright (c) 2014 Vinh Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NXVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

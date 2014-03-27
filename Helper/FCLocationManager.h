13d0609d65130a25f5b7ddb2473eed0ba24b2ec9
Vinh Nguyen <vinhnguyen2308@gmail.com>
2014-03-27 12:50:09 +0700

    Add Location Manager helper

---
+++ Helper/FCLocationManager.h
@@ -0,0 +1,45 @@
+//
+//  FCLocationManager.h
+//  Forecastr Detailed
+//
+//  Created by Rob Phillips on 4/3/13.
+//  Copyright (c) 2013 Rob Phillips. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+@import Foundation;
+@import CoreLocation;
+@import CoreLocation.CLGeocoder;
+
+@protocol FCLocationManagerDelegate <NSObject>
+- (void)didAcquireLocation:(CLLocation *)location;
+- (void)didFailToAcquireLocationWithErrorMsg:(NSString *)errorMsg;
+@optional
+- (void)didFindLocationName:(NSString *)locationName;
+@end
+
+@interface FCLocationManager : NSObject <CLLocationManagerDelegate>
+
+@property (nonatomic, weak) id <FCLocationManagerDelegate>delegate;
+
++ (id)sharedManager;
+- (void)startUpdatingLocation;
+- (void)findNameForLocation:(CLLocation *)location;
+
+@end
